e38a6ff4d2d6d4844d33e6dec7f88d6758e920b7
souzar2 <arthursouzacomp@gmail.com>
2023-06-21 19:00:33 -0300

    Script pronto

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
 let musicas = [
     {titulo:'Amarelo, azul e branco', artista:'Anavitoria', src:'musicas/01 Amarelo, azul e branco.mp3', img:'imagens/cor.jpeg'},
     {titulo:'Explodir', artista:'Anavitoria', src:'musicas/04 Explodir.mp3', img:'imagens/cor.jpeg'},
-    {titulo:'Sparks Fly', artista:'Taylor Swift', src:'musicas/02- Sparks Fly.mp3', img:'imagens/Speak NowTV.png'},
-    {titulo:'Back to December', artista:'Taylor Swift', src:'musicas/03- Back To December.mp3', img:'imagens/Speak NowTV.png'},
-    {titulo:'Speak now', artista:'Taylor Swift', src:'musicas/04- Speak Now.mp3', img:'imagens/Speak NowTV.png'},
-    {titulo:'The story of us', artista:'Taylor Swift', src:'musicas/07- The Story Of Us.mp3', img:'imagens/Speak NowTV.png'},
-    {titulo:'Enchanted', artista:'Taylor Swift', src:'musicas/09- Enchanted.mp3', img:'imagens/Speak NowTV.png'},
-    {titulo:'Long Live', artista:'Taylor Swift', src:'musicas/14- Long Live.mp3', img:'imagens/Speak NowTV.png'},
+    {titulo:'Sparks Fly (Taylor\'s Version)', artista:'Taylor Swift', src:'musicas/02- Sparks Fly.mp3', img:'imagens/Speak NowTV.png'},
+    {titulo:'Back to December (Taylor\'s Version)', artista:'Taylor Swift', src:'musicas/03- Back To December.mp3', img:'imagens/Speak NowTV.png'},
+    {titulo:'Speak now (Taylor\'s Version)', artista:'Taylor Swift', src:'musicas/04- Speak Now.mp3', img:'imagens/Speak NowTV.png'},
+    {titulo:'The story of us (Taylor\'s Version)', artista:'Taylor Swift', src:'musicas/07- The Story Of Us.mp3', img:'imagens/Speak NowTV.png'},
+    {titulo:'Enchanted (Taylor\'s Version)', artista:'Taylor Swift', src:'musicas/09- Enchanted.mp3', img:'imagens/Speak NowTV.png'},
+    {titulo:'Long Live (Taylor\'s Version)', artista:'Taylor Swift', src:'musicas/14- Long Live.mp3', img:'imagens/Speak NowTV.png'},
     {titulo:'Born this way', artista:'Lady Gaga', src:'musicas/Born This Way.mp3', img:'imagens/born this way.jpg'}
 ];
 
-
 let musica = document.querySelector('audio');
+let indexMusica = 0;
 
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
+
+renderizarMusica(indexMusica);
+
+
+// Eventos
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
 
 musica.addEventListener('timeupdate', atualizarBarra);
 
-let imagem = document.querySelector('img');
-let nomeMusica = document.querySelector('descricao h2');
-let nomeArtista = document.querySelector('descricao i');
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if (indexMusica < 0) {
+        indexMusica = 2;
+    }
+    renderizarMusica(indexMusica);
+    tocarMusica();
+});
 
-let duracaoMusica = document.querySelector('.fim');
-duracaoMusica.textContent = minutosParaSegundos(musica.duration);
+document.querySelector('.proxima').addEventListener('click', () => {
+    indexMusica++;
+    if (indexMusica > 2){
+        indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+    tocarMusica();
+});
+
+// Funções
+function renderizarMusica(index){
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}
 
 function tocarMusica(){
     musica.play();
@@ -42,11 +73,10 @@ function atualizarBarra(){
     let barra = document.querySelector('progress');
     barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
     let tempoDecorrido = document.querySelector('.inicio');
-    tempoDecorrido.textContent = minutosParaSegundos(Math.floor(musica.currentTime));
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
 }
 
-function minutosParaSegundos(segundos){
-    segundos = Math.floor(segundos);
+function segundosParaMinutos(segundos){
     let campoMinutos = Math.floor(segundos/60);
     let campoSegundos = segundos%60;
 
